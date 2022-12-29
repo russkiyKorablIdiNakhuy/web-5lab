c232df6c7ba453f37e8b07c33d6d2c3b1b5b41b1
Sencs <100563740+russkiyKorablIdiNakhuy@users.noreply.github.com>
2022-12-29 19:13:28 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,24 @@
+alert("Hello JavaScript");
+
+// HTML
+document.getElementById("demo").innerHTML = "Hello JavaScript";  
+
+// Style
+document.getElementById("demo").style.fontSize = "35px";
+document.getElementById("demo").style.display = "none";
+document.getElementById("demo").style.display = "block";
+
+var x = 5;
+var y = 6;
+var z = "Hello JavaScript";
+
+function myFunction(p1, p2) {
+    return p1 * p2; // 
+}
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: "50",
+    eyeColor: "blue"
+};
